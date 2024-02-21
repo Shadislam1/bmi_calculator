3e615b959b8cc1778557b6b0975c7c1a652ba85b
shad <shadislam999@gmail.com>
2024-02-22 00:14:34 +0600

    const color and heigt ,containar update

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,12 @@
 
 import 'package:flutter/material.dart';
 
+//color, height define
+const bottomContainerHeight = 80.0;
+const  activeCardColour =  Color(0xFF1D1E33);
+const bottomContainerColour = Color(0xFFEB1555);
+
+
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
 
@@ -29,30 +35,53 @@ class _InputPageState extends State<InputPage> {
             child: Row(
             children:<Widget> [
                Expanded(
-                child: ReusableCard(),
+                child: ReusableCard(
+                  colour: activeCardColour,
+                  ),
 
                ),
                 Expanded(
-                  child:ReusableCard(),
+                  child:ReusableCard(
+                    colour:  activeCardColour,
+                    ),
                    ),
             ],
           ),
           ),
            Expanded(
-            child: ReusableCard(),
+            child: ReusableCard(
+              colour: activeCardColour,
+              ),
                    ),
 
              Expanded(
               child: Row(
             children:<Widget> [
                Expanded(
-                child: ReusableCard(),
+                child: ReusableCard(
+                  colour:  activeCardColour,
+                  ),
                    ),
                 Expanded(
-                  child:ReusableCard(),
+                  child:ReusableCard(
+                    colour: activeCardColour, 
+                    ),
                    ),
+
+            
             ],
-          )),
+          ),
+          ),
+          //i================================================
+          // it used for bellow red shape container
+             Container(
+              color: bottomContainerColour,
+              margin: EdgeInsets.only(top: 10),
+              width: double.infinity,
+              height: bottomContainerHeight,
+             //Colors.green,
+            ),
+        
             
         ],
       )
@@ -60,10 +89,14 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+//re use row collunm functionality
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({
-    super.key,
-  });
+  //when use re useable widget we remove const key for customize owon
+
+  ReusableCard({required this.colour});
+   final Color colour;
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -71,7 +104,7 @@ class ReusableCard extends StatelessWidget {
                  
      margin: EdgeInsets.all(15.0),
      decoration: BoxDecoration(
-       color: Color(0xFF1D1E33),
+       color: colour,
       borderRadius: BorderRadius.circular(10.0),
      ),
      );
